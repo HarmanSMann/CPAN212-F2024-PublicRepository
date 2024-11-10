6da1854b22a9ebbbed26b21aac9228af9f9d0a94
Harman Mann <harmany@hotmail.ca>
2024-11-10 01:49:51 -0500

    adding starter kit for a2 - a front end

--- Class_Code/week11-sessions-JWT/02-jwts/03-JWTs-ClientEdits/server/models/user.js
+++ Class_Code/week11-sessions-JWT/02-jwts/03-JWTs-ClientEdits/server/models/user.js
@@ -1,31 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email address.']
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-  }
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "Please provide a valid email address."],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
 
-const User = mongoose.model('User', userSchema);
+    account_level: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
