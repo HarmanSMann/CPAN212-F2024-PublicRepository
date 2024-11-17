a24ba595ed8a940dc06fe4b3ddcbcd3f86cf2608
Harman Mann <harmany@hotmail.ca>
2024-11-16 23:21:48 -0500

    again

--- Class_Code/week11-sessions-JWT/04-complete_session_bookstore/index.js
+++ Class_Code/week11-sessions-JWT/04-complete_session_bookstore/index.js
@@ -12,7 +12,6 @@ dotenv.config();
 const bookRouter = require("./routes/book_router");
 const userRouter = require("./routes/user_router");
 
-
 // Initialize app
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -41,40 +40,42 @@ app.use(
 app.use("/api/books", bookRouter);
 app.use("/api/users", userRouter);
 
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
 // Static HTML routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-
 app.get("/books/all", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/allBooks.html"));
+  res.sendFile(path.join(__dirname, "public", "allBooks.html"));
 });
 
 app.get("/books/add", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/addBook.html"));
+  res.sendFile(path.join(__dirname, "public", "addBook.html"));
 });
 
 app.get("/books/edit/:id", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/editBook.html"));
+  res.sendFile(path.join(__dirname, "public", "editBook.html"));
 });
 
 app.get("/books/:id", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/bookDetail.html"));
+  res.sendFile(path.join(__dirname, "public", "bookDetail.html"));
 });
 
 // Register and Login pages
 app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/register.html"));
+  res.sendFile(path.join(__dirname, "public", "register.html"));
 });
 
 app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/login.html"));
+  res.sendFile(path.join(__dirname, "public", "login.html"));
 });
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "public/404.html"));
+  res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
 });
 
 // Database connection
@@ -89,9 +90,9 @@ db.once("open", () => {
 
 db.on("error", (err) => {
   // Check for DB errors
-  console.log("DB Error");
+  console.log("DB Error", err);
 });
 
-app.listen(PORT, () =>
+app.listen(PORT, () => 
   console.log(`Server running on http://localhost:${PORT}`)
 );
